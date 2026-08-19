b20df8f6d779fd23dfca5a508b779395bcb4c8fb
Guy Leech <guyrleech@gmail.com>
2026-08-20 00:18:29 +0100

    Added dates of first key and GUID of previous

--- Decode protect preferred fle.ps1
+++ Decode protect preferred fle.ps1
@@ -25,14 +25,16 @@ try
     $guid = New-Object System.Guid (,[byte[]]$bytes[0..15])
     $ft   = [System.BitConverter]::ToInt64($bytes, 16)
     $fileProperties = Get-ItemProperty -Path "$folder\$sid\$guid" -ErrorAction SilentlyContinue
-    $allGuidFiles = @( Get-ChildItem -Path "$folder\$sid" -File -Force | Where-Object Name -Match '^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$' | Sort-Object -Descending -Property CreationTime )
+    $allOtherGuidFiles = @( Get-ChildItem -Path "$folder\$sid" -File -Force | Where-Object { $_.Name -Match '^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$' -and $_.Name -ine $guid } | Sort-Object -Descending -Property CreationTime )
 
     [pscustomobject]@{
         "Preferred Key" = $guid
         "Rotates"       = [datetime]::FromFileTime($ft)
         "Created"       = $fileProperties | Select-Object -ExpandProperty CreationTime
-        "Previous"      = $allGuidFiles | Where-Object Name -ne $guid | Select-Object -First 1 -ExpandProperty CreationTime
-        "File Count"    = $allGuidFiles.Count
+        "Previous Created"  = $allOtherGuidFiles | Select-Object -First 1 -ExpandProperty CreationTime
+        "Previous Key"  = $allOtherGuidFiles | Select-Object -First 1 -ExpandProperty Name
+        "First Created" = $allOtherGuidFiles | Select-Object -Last 1 -ExpandProperty CreationTime
+        "File Count"    = $allOtherGuidFiles.Count
     }
 }
 catch
