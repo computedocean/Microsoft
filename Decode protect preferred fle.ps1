ca9c58d9aae3b11e8612dfc508bad75d85067077
Guy Leech <guyrleech@gmail.com>
2026-08-20 00:04:51 +0100

    Added creation time of next most recent GUID file

--- Decode protect preferred fle.ps1
+++ Decode protect preferred fle.ps1
@@ -25,11 +25,14 @@ try
     $guid = New-Object System.Guid (,[byte[]]$bytes[0..15])
     $ft   = [System.BitConverter]::ToInt64($bytes, 16)
     $fileProperties = Get-ItemProperty -Path "$folder\$sid\$guid" -ErrorAction SilentlyContinue
+    $allGuidFiles = @( Get-ChildItem -Path "$folder\$sid" -File -Force | Where-Object Name -Match '^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$' | Sort-Object -Descending -Property CreationTime )
 
     [pscustomobject]@{
-        "Preferred key" = $guid
+        "Preferred Key" = $guid
         "Rotates"       = [datetime]::FromFileTime($ft)
         "Created"       = $fileProperties | Select-Object -ExpandProperty CreationTime
+        "Previous"      = $allGuidFiles | Where-Object Name -ne $guid | Select-Object -First 1 -ExpandProperty CreationTime
+        "File Count"    = $allGuidFiles.Count
     }
 }
 catch
